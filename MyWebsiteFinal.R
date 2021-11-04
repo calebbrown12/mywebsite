f79d428697ec841651f548f5b63aa5f47ef0215e
calebbrown12 <hcb103@msstate.edu>
2021-11-03 17:13:16 -0700

    Initial commit

--- MyWebsiteFinal.R
+++ MyWebsiteFinal.R
@@ -9,3 +9,7 @@ packageVersion("postcards")
 
 library(distill)
 create_website(dir = ".", title = "mfeo", gh_pages = TRUE)
+
+create_postcard(file = "tobi.Rmd") # future name of .Rmd file
+
+create_theme("postcards")
